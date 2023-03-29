cd2aca648257b01a99e183da47c77a1f52d50a09
ricky-kiva <ricky.kiva@gmail.com>
2023-03-29 20:47:02 +0700

    add MainActvt

--- app/src/main/java/com/rickyslash/datastoreapp/MainActivity.kt
+++ app/src/main/java/com/rickyslash/datastoreapp/MainActivity.kt
@@ -1,12 +1,62 @@
 package com.rickyslash.datastoreapp
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CompoundButton
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.switchmaterial.SwitchMaterial
+
+// this extends 'dataStore' from 'Context' then create an instance of 'DataStore<Preferences>' that is backed by 'preferencesDataStore()'
+// `preferenceDataStore(name="settings")` will set the name of the dataStore
+// 'Context.dataStore' is 'delegated' by 'preferencesDataStore()'. It also means, the setter & getter are configured by 'preferencesDataStore()'
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name="settings")
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val switchTheme = findViewById<SwitchMaterial>(R.id.switch_theme)
+
+        // making a "singleton' 'instance" of SettingPreferences
+        val pref = SettingPreferences.getInstance(dataStore)
+        // making new instance of ViewModel using 'custom ViewModelFactory'
+        val mainViewModel = ViewModelProvider(this, ViewModelFactory(pref))[MainViewModel::class.java]
+
+        // observe 'isDarkModeActive' livedata (THEME_KEY in SettingPreference) from 'getThemeSettings()' and do things
+        mainViewModel.getThemeSettings().observe(this) { isDarkModeActive: Boolean ->
+            if (isDarkModeActive) {
+                // AppCompatDelegate is a class that 'allows an app' to use 'new UI features' in 'later versions of Android'
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                switchTheme.isChecked = true
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                switchTheme.isChecked = false
+            }
+        }
+
+        // update the state of 'SettingPreference' data via 'MainViewModel' 'saveThemeSetting'
+        switchTheme.setOnCheckedChangeListener { _: CompoundButton?, isChecked: Boolean ->
+            mainViewModel.saveThemeSetting(isChecked)
+        }
+
+        /* Not using this again because we use SettingPreferences
+        switchTheme.setOnCheckedChangeListener { _: CompoundButton?, isChecked: Boolean ->
+            if (isChecked) {
+                // AppCompatDelegate is a class that 'allows an app' to use 'new UI features' in 'later versions of Android'
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                switchTheme.isChecked = true
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                switchTheme.isChecked = false
+            }
+        }*/
+
     }
 }
 
